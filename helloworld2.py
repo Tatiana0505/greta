5c0347d3457a871475048983a3d615831329615c
Tatiana <tatianaa@live.fr>
2021-12-06 14:12:58 +0100

    helloworld2.py

---
+++ helloworld2.py
@@ -0,0 +1,12 @@
+#Python 3.10
+#coding : UTF-8
+
+## Helloworld.Python ##
+str = "! Helloworld !"
+print("\n"+str)
+
+str = "! Bonjour Monde !"
+print("\n"+str)
+
+str = "! test Paris !"
+print("\n"+str)
